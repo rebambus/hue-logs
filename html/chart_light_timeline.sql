05e6fd76f6f4f31ebde494438650ff7a1b687315
rebambus <39380333+rebambus@users.noreply.github.com>
2020-05-10 20:58:52 -0400

    Update chart_light_timeline.sql
    Fix it. Use better timestamps.

--- html/chart_light_timeline.sql
+++ html/chart_light_timeline.sql
@@ -1,4 +1,4 @@
-SELECT hue_lights.description row_label,
+SELECT lights.description row_label,
 CASE state WHEN 1 THEN 'On'
         /*CASE WHEN brightness >= 254 THEN 'Max'
         WHEN brightness >= 200 THEN 'Bright'
@@ -6,20 +6,19 @@ CASE state WHEN 1 THEN 'On'
         WHEN brightness >= 0 THEN 'Dim'
         ELSE 'Off' END*/
   WHEN 0 THEN 'Off' ELSE 'Unavailable' END bar_label,
-  json_date(CONVERT_TZ(CASE WHEN start_time > TIMESTAMPADD(HOUR,-24,UTC_TIMESTAMP()) THEN start_time
-                            ELSE TIMESTAMPADD(HOUR,-24,UTC_TIMESTAMP()) END
-                            ,'+00:00',@@global.time_zone)) start_time,
-  json_date(CONVERT_TZ(end_time,'+00:00',@@global.time_zone)) end_time
-FROM hue_light_history
-JOIN hue_lights ON hue_lights.light_id = hue_light_history.light_id
+  UNIX_TIMESTAMP(CASE WHEN start_time > TIMESTAMPADD(HOUR,-24,UTC_TIMESTAMP()) THEN start_time
+                      ELSE TIMESTAMPADD(HOUR,-24,UTC_TIMESTAMP()) END) start_time,
+  UNIX_TIMESTAMP(end_time) end_time
+FROM light_history AS log
+JOIN lights ON lights.id = log.light_id
 LEFT JOIN
     (SELECT light_id,
           SUM(CASE WHEN state = 1 THEN TIMESTAMPDIFF(SECOND,start_time,end_time) ELSE 0 END) total_time_on,
           SUM(CASE WHEN state = 0 THEN TIMESTAMPDIFF(SECOND,start_time,end_time) ELSE 0 END) total_time_off
-      FROM hue_light_history
+      FROM light_history
       WHERE start_time > TIMESTAMPADD(HOUR,-24,UTC_TIMESTAMP())
       GROUP BY light_id
-    ) light_summary ON light_summary.light_id = hue_light_history.light_id
+    ) light_summary ON light_summary.light_id = log.light_id
 WHERE end_time > TIMESTAMPADD(HOUR,-24,UTC_TIMESTAMP())
     AND state IS NOT NULL
   -- AND state=1
