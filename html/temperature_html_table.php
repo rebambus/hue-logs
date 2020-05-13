1fa1e31b2a7254b3e873cbc9c965f07205731fdc
rebambus <39380333+rebambus@users.noreply.github.com>
2020-05-13 12:42:02 -0400

    Update temperature_html_table.php

--- html/temperature_html_table.php
+++ html/temperature_html_table.php
@@ -4,13 +4,13 @@ require_once('mysqli_connect.php');
 $sql = "
 SELECT   sensor.sensor_id,
          sensor.description,
-         ROUND(log.value, 1) AS temperature,
+         ROUND(log.value, 0) AS temperature,
          UNIX_TIMESTAMP(log.lastupdated) AS time,
-         ROUND(log_min.value, 1) AS min_temp,
-         ROUND(log_min.value - log.value, 1) AS min_diff,
+         ROUND(log_min.value, 0) AS min_temp,
+         ROUND(log_min.value - log.value, 0) AS min_diff,
          UNIX_TIMESTAMP(log_min.lastupdated) AS min_time,
-         ROUND(log_max.value, 1) AS max_temp,
-         ROUND(log_max.value - log.value, 1) AS max_diff,
+         ROUND(log_max.value, 0) AS max_temp,
+         ROUND(log_max.value - log.value, 0) AS max_diff,
          UNIX_TIMESTAMP(log_max.lastupdated) AS max_time
 FROM     hue_sensor_data AS log
          JOIN hue_sensors AS sensor
@@ -77,7 +77,7 @@ if ($result->num_rows > 0) {
 				echo '<td><span title="'. $row['min_time']. '"><script>document.write(moment.unix("'. $row['min_time']. '").calendar());</script></span></td>';
 				echo '<td><span title="'. $row['min_time']. '" data-livestamp="'. $row['min_time']. '"></span></td>';
 				echo "<td>" . $row["max_temp"] . "</td>";
-				echo "<td>" . $row["max_diff"] . "</td>";
+				echo "<td>+" . $row["max_diff"] . "</td>";
 				echo '<td><span title="'. $row['max_time']. '"><script>document.write(moment.unix("'. $row['max_time']. '").calendar());</script></span></td>';
 				echo '<td><span title="'. $row['max_time']. '" data-livestamp="'. $row['max_time']. '"></span></td>';
 				echo "</tr>\n";
