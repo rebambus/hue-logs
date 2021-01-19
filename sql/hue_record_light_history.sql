00f0093d761b44b4a4460b7cda38931d29b2d40f
Tom Butler <39380333+rebambus@users.noreply.github.com>
2021-01-19 18:55:01 -0500

    Fix a significant bug, I think?

--- sql/hue_record_light_history.sql
+++ sql/hue_record_light_history.sql
@@ -8,8 +8,8 @@ IF @light_id IS NULL THEN
 	INSERT INTO lights (description, uniqueid)
 	VALUES (description, uniqueid);
 
-	SELECT @light_id := id
-	FROM lights
+	SELECT @light_id := lights.id
+	FROM lights lights
 	WHERE lights.uniqueid = uniqueid;
 END IF;
 
@@ -18,27 +18,25 @@ SET description = description
 WHERE id = @light_id;
 
 -- new
-SELECT	@current_id := id,
-	@current_state := state,
-	@current_brightness := brightness,
-	@current_reachable := reachable,
-	@current_end_time := end_time
-FROM light_history
-WHERE light_id = @light_id
-ORDER BY id DESC
+SELECT	@current_id := lh.id,
+	@current_state := lh.state,
+	@current_brightness := lh.brightness,
+	@current_reachable := lh.reachable,
+	@current_end_time := lh.end_time
+FROM light_history lh
+WHERE lh.light_id = @light_id
+ORDER BY lh.id DESC
 LIMIT 1;
 
 IF	@current_state = state
 	AND @current_brightness = brightness
 	AND @current_reachable = reachable
-	AND @current_end_time >= DATE_ADD(UTC_TIMESTAMP(), INTERVAL -5 MINUTE) THEN
-BEGIN
+	AND @current_end_time >= DATE_ADD(UTC_TIMESTAMP(), INTERVAL -5 MINUTE)
+THEN
 	UPDATE light_history
 	SET end_time = UTC_TIMESTAMP()
 	WHERE id = @current_id;
-END
 ELSE
-BEGIN
 	INSERT INTO light_history (light_id, start_time, end_time, state, brightness, reachable)
 	VALUES (@light_id, UTC_TIMESTAMP(), UTC_TIMESTAMP(), state, brightness, reachable);
 END IF;
