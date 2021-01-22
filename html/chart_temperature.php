ea7c1b70830360ca7b63c8def25574c6231c6f3e
Tom Butler <39380333+rebambus@users.noreply.github.com>
2021-01-22 14:29:30 -0500

    Rename sensors. This still isn't dynamic...

--- html/chart_temperature.php
+++ html/chart_temperature.php
@@ -29,24 +29,24 @@ if (isset($_GET['output'])) {
         echo '<table>';
         echo '<tr>';
         echo '<th>Time</th>';
-        echo '<th>Basement</th>';
-        echo '<th>Upstairs Hallway</th>';
-        echo '<th>Bathroom</th>';
-        echo '<th>Front Porch</th>';
-        echo '<th>Front Hallway</th>';
         echo '<th>Back Porch</th>';
-        echo '<th>Outdoor</th>';
+        echo '<th>Front Porch</th>';
+        echo '<th>Basement</th>';
+        echo '<th>Bathroom</th>';
+        echo '<th>Front Hallway</th>';
+        echo '<th>Living Room</th>';
+        echo '<th>Upstairs Hallway</th>';
         echo '</tr>';
         while ($row = $result->fetch_assoc()) {
             echo '<tr>';
             echo '<td>'. $row['lastupdated']. '</td>';
-            echo '<td>'. $row['basement']. '</td>';
-            echo '<td>'. $row['upstairs_hallway']. '</td>';
-            echo '<td>'. $row['bathroom']. '</td>';
-            echo '<td>'. $row['front_porch']. '</td>';
-            echo '<td>'. $row['front_hallway']. '</td>';
             echo '<td>'. $row['back_porch']. '</td>';
-            echo '<td>'. $row['outdoor']. '</td>';
+            echo '<td>'. $row['front_porch']. '</td>';
+            echo '<td>'. $row['basement']. '</td>';
+            echo '<td>'. $row['bathroom']. '</td>';
+            echo '<td>'. $row['front_hallway']. '</td>';
+            echo '<td>'. $row['living_room']. '</td>';
+            echo '<td>'. $row['upstairs_hallway']. '</td>';
             echo '</tr>';
         }
         echo '</table>';
@@ -56,14 +56,14 @@ if (isset($_GET['output'])) {
 
 $table = array();
 $table['cols'] = array(
-  array('id' => 'Last Updated',	'label' => 'Last Updated',	'type' => 'datetime',	'role' => 'domain'),
+  array('id' => 'Last_Updated',	'label' => 'Last Updated',	'type' => 'datetime',	'role' => 'domain'),
+  array('id' => 'Back_Porch',	'label' => 'Back Porch',	'type' => 'number'),
+  array('id' => 'Front_Porch',	'label' => 'Front Porch',	'type' => 'number'),
   array('id' => 'Basement',	'label' => 'Basement',	'type' => 'number'),
-  array('id' => 'Upstairs_Hallway',	'label' => 'Upstairs_Hallway',	'type' => 'number'),
   array('id' => 'Bathroom',	'label' => 'Bathroom',	'type' => 'number'),
-  array('id' => 'Front_Porch',	'label' => 'Front_Porch',	'type' => 'number'),
-  array('id' => 'Front_Hallway',	'label' => 'Front_Hallway',	'type' => 'number'),
-  array('id' => 'Back_Porch',	'label' => 'Back_Porch',	'type' => 'number'),
-  array('id' => 'Outdoor',	'label' => 'Outdoor',	'type' => 'number')
+  array('id' => 'Front_Hallway',	'label' => 'Front Hallway',	'type' => 'number'),
+  array('id' => 'Living_Room',	'label' => 'Living Room',	'type' => 'number'),
+  array('id' => 'Upstairs_Hallway',	'label' => 'Upstairs Hallway',	'type' => 'number')
 );
 
 $rows = array();
@@ -72,13 +72,13 @@ if ($result->num_rows > 0) {
         $temp = array();
 
         $temp[] = array('v' => 'Date(' . 1000*$row['lastupdated'] .')');
-        $temp[] = array('v' => $row['basement']);
-        $temp[] = array('v' => $row['upstairs_hallway']);
-        $temp[] = array('v' => $row['bathroom']);
-        $temp[] = array('v' => $row['front_porch']);
-        $temp[] = array('v' => $row['front_hallway']);
         $temp[] = array('v' => $row['back_porch']);;
-        $temp[] = array('v' => $row['outdoor']);
+        $temp[] = array('v' => $row['front_porch']);
+        $temp[] = array('v' => $row['basement']);
+        $temp[] = array('v' => $row['bathroom']);
+        $temp[] = array('v' => $row['front_hallway']);
+        $temp[] = array('v' => $row['living_room']);
+        $temp[] = array('v' => $row['upstairs_hallway']);
 
         $rows[] = array('c' => $temp);
         ;
--- html/chart_temperature.sql
+++ html/chart_temperature.sql
@@ -2,22 +2,22 @@
 
 SELECT
 		UNIX_TIMESTAMP(lastupdated) AS lastupdated,
-		ROUND(basement,2) basement,
-		ROUND(upstairs_hallway,2) upstairs_hallway,
-		ROUND(bathroom,2) bathroom,
-		ROUND(front_porch,2) front_porch,
-		ROUND(front_hallway,2) front_hallway,
 		ROUND(back_porch,2) back_porch,
-		ROUND(outdoor,2) outdoor
+		ROUND(front_porch,2) front_porch,
+		ROUND(basement,2) basement,
+		ROUND(bathroom,2) bathroom,
+		ROUND(front_hallway,2) front_hallway,
+		ROUND(living_room,2) living_room,
+		ROUND(upstairs_hallway,2) upstairs_hallway
 FROM (SELECT
 	 			FROM_UNIXTIME(MAX(UNIX_TIMESTAMP(lastupdated))) lastupdated,
-				AVG(CASE description WHEN 'Basement'         THEN value END) AS basement,
-				AVG(CASE description WHEN 'Upstairs Hallway' THEN value END) AS upstairs_hallway,
-				AVG(CASE description WHEN 'Bathroom'         THEN value END) AS bathroom,
-				AVG(CASE description WHEN 'Front Porch'      THEN value END) AS front_porch,
-				AVG(CASE description WHEN 'Front Hallway'    THEN value END) AS front_hallway,
 				AVG(CASE description WHEN 'Back Porch'       THEN value END) AS back_porch,
-				AVG(CASE description WHEN 'Outdoor Sensor'   THEN value END) AS outdoor
+				AVG(CASE description WHEN 'Front Porch'      THEN value END) AS front_porch,
+				AVG(CASE description WHEN 'Basement'         THEN value END) AS basement,
+				AVG(CASE description WHEN 'Bathroom'         THEN value END) AS bathroom,
+				AVG(CASE description WHEN 'Front Hallway'    THEN value END) AS front_hallway,
+				AVG(CASE description WHEN 'Living Room'      THEN value END) AS living_room,
+				AVG(CASE description WHEN 'Upstairs Hallway' THEN value END) AS upstairs_hallway
 		FROM hue_sensor_data
 		JOIN hue_sensors ON hue_sensors.sensor_id = hue_sensor_data.sensor_id
 		WHERE type = 'temperature'
