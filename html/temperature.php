2af92366edd5063365d488afc83bb37df12ba940
Tom Butler <39380333+rebambus@users.noreply.github.com>
2021-05-28 10:42:27 -0400

    Try adding error logging

--- html/temperature.php
+++ html/temperature.php
@@ -1,3 +1,8 @@
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 <script type="text/javascript" src="http://momentjs.com/downloads/moment.js"></script>
 <h1 class="bd-title">Temperature</h1>
 
